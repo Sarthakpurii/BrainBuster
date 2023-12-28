fa02a63d12bab119b982d4d00a288654e84f4f45
Sarthak <sarthak.00puri@gmail.com>
2023-12-29 03:22:21 +0530

    added questions.dart

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -19,7 +19,22 @@ class GradientContainer extends StatelessWidget{
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Image.asset('assets/images/QuizIcon.png',)
+                Image.asset('assets/images/QuizIcon.png',width: 300,),
+                const SizedBox(height: 50,),
+                const Text('Learn Flutter the Fun Way!',style: TextStyle(
+                  // fontFamily: ,
+                  fontSize: 20
+                ),
+                ),
+                const SizedBox(height: 30,),
+                OutlinedButton.icon(
+                  onPressed: (){}, 
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 236, 216, 39),
+                    elevation: 10,
+                    ),
+                    icon: const Icon(Icons.timer_outlined),
+                  label: const Text('Start Quiz'))
               ],
             ),
           ),
